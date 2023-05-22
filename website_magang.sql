a9362e49a8bba441e387657e2db614293f7d823e
Andre Ivan Permana <111367872+mandragid@users.noreply.github.com>
2023-05-22 21:20:51 +0700

    Add files via upload
    database

---
+++ website_magang.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 22, 2023 at 04:20 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `website_magang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_mahasiswa`
+--
+
+CREATE TABLE `data_mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `jurusan` varchar(30) NOT NULL,
+  `semester` varchar(2) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_hp` int(15) NOT NULL,
+  `periode_mulai` varchar(10) NOT NULL,
+  `periode_akhir` varchar(10) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `universitas` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_mahasiswa`
+--
+ALTER TABLE `data_mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_mahasiswa`
+--
+ALTER TABLE `data_mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
